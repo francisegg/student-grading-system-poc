edd440df4ae034e2435937b1a5164c3d940c3b0c
francisegg <bm6hjj2020.tw@gmail.com>
2026-01-07 02:12:51 +0000

    add dockerfile and set up to Cloudflare

--- main.go
+++ main.go
@@ -56,7 +56,7 @@ func init() {
 	googleOauthConfig = &oauth2.Config{
 		ClientID:     os.Getenv("GOOGLE_CLIENT_ID"),
 		ClientSecret: os.Getenv("GOOGLE_CLIENT_SECRET"),
-		RedirectURL:  "http://localhost:8080/auth/callback",
+		RedirectURL:  "http://www.teaegg.space/auth/callback",
 		Scopes:       []string{"https://www.googleapis.com/auth/userinfo.email", "https://www.googleapis.com/auth/userinfo.profile"},
 		Endpoint:     google.Endpoint,
 	}
